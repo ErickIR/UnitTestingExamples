b1c85259d1918d097e77523624d58384c70427e2
ErickIR <erickrc9827@gmail.com>
2021-04-11 08:44:48 -0400

    Add project files.

---
+++ UnitTestingExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitTestingExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
