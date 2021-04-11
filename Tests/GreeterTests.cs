2ec71ac3aadbe8a1859f16fce56d5e7319822760
ErickIR <erickrc9827@gmail.com>
2021-04-11 09:16:11 -0400

    bad-greeter

--- Tests/GreeterTests.cs
+++ Tests/GreeterTests.cs
@@ -1,18 +1,44 @@
 using NUnit.Framework;
+using UnitTestingExamples;
 
 namespace Tests
 {
     public class GreeterTests
     {
         [Test]
-        public void Greet_SaysGoodMorning_WhenTimeIsBefore12PM()
+        public void Greet_SaysGoodMorning_WhenHourIsBetween5AMAnd12PM()
         {
             // Arrange
             var sut = new Greeter();
             // Act
+            //var result = sut.Greet(new System.DateTime(2020, 01, 01, 6, 0, 0));
             var result = sut.Greet();
             // Assert
-            Assert.AreEqual(result, "Good Morning");
+            Assert.AreEqual("Good Morning", result);
+        }
+
+        [Test]
+        public void Greet_SaysGoodAfternoon_WhenHourIsBetween12PMAnd6Pm()
+        {
+            // Arrange
+            var sut = new Greeter();
+            // Act
+            //var result = sut.Greet(new System.DateTime(2020, 01, 01, 15, 0,0));
+            var result = sut.Greet();
+            // Assert
+            Assert.AreEqual("Good Afternoon", result);
+        }
+
+        [Test]
+        public void Greet_SaysGoodNight_WhenHourIsPast6Pm()
+        {
+            // Arrange
+            var sut = new Greeter();
+            // Act
+            //var result = sut.Greet(new System.DateTime(2020, 01, 01, 19, 0, 0));
+            var result = sut.Greet();
+            // Assert
+            Assert.AreEqual("Good Night", result);
         }
     }
 }
---
+++ UnitTestingExamples/Greeter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestingExamples
+{
+    public class Greeter
+    {
+        public string Greet()
+        {
+            var dateTime = DateTime.Now;
+            if (dateTime.Hour > 5 && dateTime.Hour < 12)
+                return "Good Morning";
+            if (dateTime.Hour >= 12 && dateTime.Hour < 18)
+                return "Good Afternoon";
+            else
+                return "Good Night";
+        }
+    }
+}
